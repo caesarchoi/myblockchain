4fdf77afaed53efdd3949d4a17a36707cb1ff48d
caesarchoi <gecjh@naver.com>
2018-04-24 21:35:48 +0900

    2018.04.24

---
+++ server.rb
@@ -0,0 +1,14 @@
+# encoding: UTF-8
+
+require 'sinatra'
+require 'sinatra/reloder'
+
+get '/mining' do
+   "<h1 style='text-align:center'>회원가입 페이지 입니다.</h1>"
+
+ end
+
+ get '/make_a_transaction' do
+   "회원가입 페이지 입니다."
+
+ end
